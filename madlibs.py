d9fa6d99245d37f2605a1b71ece061cffe57f14f
roangira99 <angirarobert7@gmail.com>
2023-01-30 09:58:05 +0300

    madlibs game

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+desc = input("Description: ")
+action = input("Action: ")
+action2 = input("Action: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {desc}! It makes me so excited because \
+FunI love to {action}. Stay hydrated and {action2} like you are {famous_person}!"
+
+print(madlib)
---
+++ random_madlibs.py
@@ -0,0 +1,6 @@
+import hp, mycode, zombie, hungergames
+import random
+
+if __name__ == "__main__":
+    m = random.choice([hp, mycode, zombie, hungergames])
+    m.madlib()
